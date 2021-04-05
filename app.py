cba3f826432ac12451a65b1febb879b4d00ffc97
abhishekbhagwat <abhishek027@e.ntu.edu.sg>
2021-04-06 02:03:53 +0800

    Added Fine Grained Sentiment Analysis ML model

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 from sentiment_classifier.model import Model, get_model
+from fine_grained_sentiment_classifier.model import FineGrainedModel, get_fine_grained_model
 import re
 import spacy_streamlit
 import en_core_web_sm
@@ -22,10 +23,17 @@ def main():
     sentiment_filter = st.sidebar.multiselect('Sentiment of content', ['Positive', 'Negative', 'Neutral'])
     emotion_filter = st.sidebar.multiselect('Emotion of content', ['Happy', 'Sad', 'Angry', 'Confused'])
     model = get_model()
+    fine_grained_model = get_fine_grained_model()
 
     sentiment, confidence, probabilities = model.predict(search_query)
+    fg_sentiment, fg_confidence, fg_probabilities = fine_grained_model.predict(search_query)
+
     st.markdown('__Sentiment__ of this review is: `{0}`.'.format(sentiment))
+    st.markdown('__Fine Grained Sentiment__ of this review is: `{0}`.'.format(fg_sentiment))
+
     # st.markdown('The confidence probabilities of the classes are `{0}`'.format(probabilities))
+    # st.markdown('The confidence probabilities of the classes are `{0}`'.format(fg_probabilities))
+
 
     # st.write(search_query, search_result_number, sentiment_filter, emotion_filter)
 
